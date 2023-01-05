9042aa36f9671f153f55a457bb4b4ac050cc9faf
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2023-01-05 16:31:42 +0300

    test create user in database

--- domain/entities/user_entity.py
+++ domain/entities/user_entity.py
@@ -1,10 +1,17 @@
 
-
+from datetime import datetime
 from domain.entities.base_entity import BaseEntity
 
 
 class User(BaseEntity):
+    username: str
+    password: str
+    name: str
 
-    username:str
-    password:str
-    name:str
+    def __init__(self, username, name, password=None) -> None:
+        super().__init__()
+        self.username = username
+        self.name = name
+        self.password = password
+        if not hasattr(self, '_id'):
+            self.createdAt = datetime.now()
